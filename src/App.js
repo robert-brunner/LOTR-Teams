e975bf893583e37f7ed31811f0460bc8685caa6f
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-02-24 12:16:00 -0500

    newer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { CurrentCharacterItem } from './components/characters/characterItem';
+import { InfoList } from './components/society/apiList';
 
 function App() {
   return (
     <div className="App">
       <CurrentCharacterItem/>
+      <InfoList/>
     </div>
   );
 }
